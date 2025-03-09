fea4b2f696d1264958ceff5f2e04124d3920b1a9
ManuxDark <mgcastelberg@hotmail.com>
2025-03-08 22:58:03 -0600

    Login Page Diseño Segunda Parte

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
-import { TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
+import { Button, TextField, Typography, Link } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import React from 'react'
+import { Link as RouterLink } from 'react-router';
 
 export const LoginPage = () => {
   return (
@@ -13,21 +15,42 @@ export const LoginPage = () => {
       justifyContent={"center"}
       sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
     >
-      <Grid item 
+      <Grid  
         className="box-shadow" 
-        xs={3} 
-        sx={{ width: { sm: 450 }, backgroundColor: "rgba(256,256,256,0.9)", padding: 3, borderRadius: 2 }}>
+        size={3}
+        sx={{ width: { sm: 450, xs: 300 }, backgroundColor: "rgba(256,256,256,0.9)", padding: 3, borderRadius: 2 }}>
 
           <Typography variant="h5" sx={{ mb: 1 }}>Login</Typography>
 
           <form>
             <Grid container>
-              <Grid item size={12} sx={{ mt: 2 }}>
-                <TextField label="Correo" fullWidth type="email" placeholder="Correo" />
+              <Grid  size={12} sx={{ mt: 2 }}>
+                <TextField label="Correo" fullWidth type="email" />
               </Grid>
-              <Grid item size={12} sx={{ mt: 2 }}>
-                <TextField label="Contraseña" fullWidth type="password" placeholder="Contraseña" />
+              <Grid  size={12} sx={{ mt: 2 }}>
+                <TextField label="Contraseña" fullWidth type="password" />
               </Grid>
+
+              <Grid container size={12} spacing={2} sx={{ mb: 1, mt: 2 }}>
+                <Grid size={{ xs: 12, md: 6 }}>
+                  <Button fullWidth variant="outlined" >
+                    <Google /> 
+                    <Typography sx={{ ml: 1, fontSize: 15 }}>Google</Typography>
+                  </Button>
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                  <Button fullWidth variant="contained" >Login</Button>
+                </Grid>
+              </Grid>
+
+              <Grid container size={12} direction="row" sx={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                  mt: 1
+              }}>
+                <Link component={RouterLink} to="/auth/register">Crear una cuenta</Link>
+              </Grid>
+              
             </Grid>
           </form>
 
