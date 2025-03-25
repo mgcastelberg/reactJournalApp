d8c7a7da32d215af4bdffa834a33fb9400f91e88
ManuxDark <mgcastelberg@hotmail.com>
2025-03-25 10:39:13 -0600

    Configurando Redux en nuestra aplicación JournalApp

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import './styles.css'
 import { BrowserRouter, Routes } from 'react-router'
 import { AppRouter } from './router/AppRouter'
 import { JournalApp } from './JournalApp'
+import { Provider } from 'react-redux'
+import { store } from './store'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-        <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+          <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
---
+++ src/store/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit'
+// Fuente unica de la verdad
+export const store = configureStore({
+  reducer: {
+    
+  },
+})
