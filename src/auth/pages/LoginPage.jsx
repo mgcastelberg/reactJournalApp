45eba231925b8152bacf16fee6ce40637836940d
ManuxDark <mgcastelberg@hotmail.com>
2025-03-26 09:02:25 -0600

    Manejo de formulario de login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -3,30 +3,52 @@ import { Button, TextField, Typography, Link } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import { Link as RouterLink } from 'react-router';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+import { useDispatch } from 'react-redux';
+import { checkingAuthentication } from '../../store/auth/thunks';
 
 export const LoginPage = () => {
+
+  const dispach = useDispatch();
+
+  const { email, password, onInputChange } = useForm({
+    'email': 'jmgc@terraunida.com',
+    'password': '123456'
+  });
+
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log({ email, password });
+    dispach( checkingAuthentication() );
+  }
+
+  const onGoogleSignIn = () => {
+    console.log('onGoogleSignIn');
+    dispach( checkingAuthentication() );
+  }
+
   return (
 
     <AuthLayout title="Login">
 
-          <form>
+          <form onSubmit={ onSubmit }>
             <Grid container>
               <Grid  size={12} sx={{ mt: 2 }}>
-                <TextField label="Correo" fullWidth type="email" />
+                <TextField label="Correo" fullWidth type="email" name='email' value={ email } onChange={ onInputChange } />
               </Grid>
               <Grid  size={12} sx={{ mt: 2 }}>
-                <TextField label="Contraseña" fullWidth type="password" />
+                <TextField label="Contraseña" fullWidth type="password" name='password' value={ password } onChange={ onInputChange } autoComplete='off' />
               </Grid>
 
               <Grid container size={12} spacing={2} sx={{ mb: 1, mt: 2 }}>
                 <Grid size={{ xs: 12, md: 6 }}>
-                  <Button fullWidth variant="outlined" >
+                  <Button fullWidth variant="outlined" onClick={ onGoogleSignIn } >
                     <Google /> 
                     <Typography sx={{ ml: 1, fontSize: 15 }}>Google</Typography>
                   </Button>
                 </Grid>
                 <Grid size={{ xs: 12, md: 6 }}>
-                  <Button fullWidth variant="contained" >Login</Button>
+                  <Button type='submit' fullWidth variant="contained" >Login</Button>
                 </Grid>
               </Grid>
 
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        status: 'checking', // 'authenticated', 'not-authenticated'
+        status: 'not-authenticated', // 'authenticated', 'not-authenticated', checking
         uid: null,
         email: null,
         displayName: null,
@@ -18,7 +18,7 @@ export const authSlice = createSlice({
             // state.counter += 1;
         },
         checkingCredentials: (state) => {
-            // state.status = 'checking';
+            state.status = 'checking';
         },
     },
 });
