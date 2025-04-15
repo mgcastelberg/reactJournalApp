3f7b47357c3d5834d9e15e067137a5d9117a17ad
ManuxDark <mgcastelberg@hotmail.com>
2025-04-15 09:15:55 -0600

    Custom Hook para autenticación

---
+++ src/hooks/useCheckAuth.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { FirebaseAuth } from '../firebase/config';
+import { login, logout } from '../store/auth/authSlice';
+import { onAuthStateChanged } from 'firebase/auth';
+
+export const useCheckAuth = () => {
+    const { status } = useSelector( state => state.auth );
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      onAuthStateChanged( FirebaseAuth, async( user ) => {
+        // console.log(user);
+        if ( !user ) return dispatch( logout() );
+  
+        const { uid, displayName, email, photoURL } = user;
+  
+        dispatch( login({ uid, displayName, email, photoURL }) );
+  
+      })
+    }, []);
+
+    return status;
+}
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,12 @@
-import React, { useEffect } from "react";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 import { Navigate, Route, Routes } from "react-router";
-import { useDispatch, useSelector } from "react-redux";
 import { CheckingAuth } from "../ui";
-import { onAuthStateChanged } from "firebase/auth";
-import { FirebaseAuth } from "../firebase/config";
-import { login, logout } from "../store/auth/authSlice";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
   
-  const { status } = useSelector( state => state.auth );
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged( FirebaseAuth, async( user ) => {
-      // console.log(user);
-      if ( !user ) return dispatch( logout() );
-
-      const { uid, displayName, email, photoURL } = user;
-
-      dispatch( login({ uid, displayName, email, photoURL }) );
-
-    })
-  }, []);
+  const status  = useCheckAuth();
 
   if ( status === 'checking' ) {
     return <CheckingAuth />;
