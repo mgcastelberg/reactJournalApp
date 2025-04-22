e84e3a711fc4219dc20439ff880301d0526afa4d
ManuxDark <mgcastelberg@hotmail.com>
2025-04-22 08:36:18 -0600

    JournalSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authSlice } from './auth'
+import { journalSlice } from './journal'
 // Fuente unica de la verdad
 export const store = configureStore({
   reducer: {
-    auth: authSlice.reducer
+    auth: authSlice.reducer,
+    journal: journalSlice.reducer
   },
 })
